abb5faf4fde8558c89f46971909a259578bd6a18
Jet <jethro.talavra1@icloud.com>
2020-10-31 22:39:48 -0400

    added json web token for authentication

--- routes/auth.js
+++ routes/auth.js
@@ -2,20 +2,23 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const config = require('config')
+const User = require("../model/User")
 const {check, validationResult} = require('express-validator')
 
 
-// End Point -> api/auth GET REQUEST
-// Description - > GET LOGGED IN UER
-// ACCESS - > PRIVATE ACCESS
-router.get("/",
+// End Point ->   POST REQUEST api/auth
+// Description - > AUTHORIZE USER AND GET TOKEN
+// ACCESS - > PUBLIC ACCESS
+router.post("/",
 [
     check("email", "please include valid email").isEmail(),
     check("password", "please include a password").exists()
 ],
 async (req, res) => {
-    const errors = validationResult();
+    const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({errors: errors.array() })
     }
@@ -51,16 +54,19 @@ async (req, res) => {
 
     } catch (error) {
         console.error(error.message)
-        res.status(500).send("server error")
+
+        res.status(500).send("server error" )
     }
 })
 
 
-// End Point ->   POST REQUEST api/auth
-// Description - > AUTHORIZE USER AND GET TOKEN
-// ACCESS - > PUBLIC ACCESS
-router.post("/", (req, res) => {
-    res.send("LOG IN USER")
+
+
+// End Point -> api/auth GET REQUEST
+// Description - > GET LOGGED IN UER
+// ACCESS - > PRIVATE ACCESS
+router.get("/", (req, res) => {
+    res.send("GET LOGGED IN USER")
 })
 
 module.exports = router;
